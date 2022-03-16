684205b9addbadda5bd94430aa7cf1a3ef2eb50c
tudorducaru <tducaru@gmail.com>
2022-03-16 22:58:43 +0200

    add logs

--- client/src/helpers/sorting.js
+++ client/src/helpers/sorting.js
@@ -17,6 +17,8 @@ export const sortProjects = (projects, sort_field, sort_direction) => {
             a[sort_field] = a[sort_field] ? a[sort_field] : '';
             b[sort_field] = b[sort_field] ? b[sort_field] : '';
 
+            console.log(a[sort_field], b[sort_field]);
+
             if (sort_direction === 'ASC') {
                 return a[sort_field].localeCompare(b[sort_field]);
             } else {
