0c07d9f71a83fcd3758869b938df9aa88ba56217
tudorducaru <tducaru@gmail.com>
2022-04-04 09:34:31 +0300

    create method to establish db connection

--- db.js
+++ db.js
@@ -1,22 +1,29 @@
 const mysql = require('mysql');
 
 // Create new database connection
-const connection = mysql.createConnection({
+const dbConnection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
 
-// Connect to the database
-connection.connect(function(err) {
-    if (err) {
-        console.log('Error connecting to MySQL');
-        console.log(err);
-    } else {
-        console.log('Connected successfully to MySQL');
-    }
-})
+const getDbConnection = () => {
+    
+    // Connect to the database
+    dbConnection.connect(function (err) {
+        if (err) {
+            console.log('Error connecting to MySQL');
+            console.log(err);
+        } else {
+            console.log('Connected successfully to MySQL');
+        }
+    });
+
+}
 
 // Export the connection
-module.exports = connection;
+module.exports = {
+    dbConnection,
+    getDbConnection
+};
